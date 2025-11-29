54f3eab85b9ee584717806f3087591ac9dbc8c4b
karMareal1 <kareemtaher25@gmail.com>
2025-11-29 16:55:01 -0500

    created the main.py route /contacts with method of GET

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,15 @@
 from flask import request, jsonify
 from config import app, db
 from models import Contact
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()  #fetch all contacts from database
+    json_contacts = list(map(lambda x:x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})    
+
+#since when importing comes with the app running immediatly, this only focuses on running the app if only this file was run
+if __name__ == "__main__":
+    with app.app_context():
+        app.create_all() #creating the database
+    app.run(debug=True)
